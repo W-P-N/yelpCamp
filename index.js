6d340df6811212d034171a69ed0044c3ba31c4fa
Parth <parthwani22@gmail.com>
2024-07-08 16:50:35 +0530

    Added layout using ejsmate.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 // Models
 const Campground = require('./models/campGround');
@@ -18,6 +19,7 @@ async function connectDb() {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
