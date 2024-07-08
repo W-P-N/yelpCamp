2daca91c7cc77e066c425ca5c6a836df43d1a4e6
Parth <parthwani22@gmail.com>
2024-07-08 12:20:10 +0530

    Added seed file

---
+++ seeds/index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+
+const { places, descriptors } = require('./seedHelpers');
+
+// Models
+const Campground = require('./../models/campGround');
+
+// Connect Local Database
+connectDb().catch(err => console.log(err));
+
+// Function to connect Local db
+async function connectDb() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {}).then(() => {
+    console.log("Database Connected");
+  })
+}
+
+const sample = arr => arr[Math.floor(Math.random() * arr.length)];
+
+const seedDb = async () => {
+    await Campground.deleteMany({});
+    for (let i=0; i<50; i++) {
+        const randomNum = Math.floor(Math.random() * 212 + 1);
+        const camp = new Campground({
+            location: `${cities[randomNum].city}, ${cities[randomNum].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+        })
+        await camp.save();
+    }
+}
+
+seedDb().then( () => {
+    mongoose.connection.close();
+}
+);
