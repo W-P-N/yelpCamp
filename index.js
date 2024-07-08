c0e29d47a7fa0436c892b1ba0bf30b85bea626f4
Parth <parthwani22@gmail.com>
2024-07-08 12:25:47 +0530

    Added campgrounds directory and code

--- index.js
+++ index.js
@@ -24,12 +24,9 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({
-        title: "Backyard"
-    })
-    await camp.save()
-    res.send("Camp Created");
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds})
 })
 
 app.listen(3000, () => {
