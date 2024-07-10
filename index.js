743213c1b1a83f4d2de9f916af79b3de6abc0dbb
Parth <parthwani22@gmail.com>
2024-07-10 10:12:44 +0530

    Added Joi library

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+const Joi = require('joi');
 
 
 // Models
@@ -48,6 +49,21 @@ app.get('/campgrounds/:id', catchAsync(async (req, res) => {
 }))
 
 app.post('/campgrounds', catchAsync(async (req, res) => {
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required()
+        }).required()
+    })
+    const { error }= campgroundSchema.validate(req.body);
+
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
     const campground = new Campground(req.body.campground);
     if(!campground) throw new ExpressError("Invalid Campground data", 400);
     await campground.save();
