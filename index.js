0422841b77cb937fd3ba3b0501258e97acf45b3f
Parth <parthwani22@gmail.com>
2024-07-17 09:02:19 +0530

    Mongo sanitize to prevent mongo injection.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const mongoSanitize = require('express-mongo-sanitize');
 
 // Models
 const User = require('./models/user');
@@ -40,6 +41,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
     secret: 'thisissecret',
@@ -64,7 +66,7 @@ passport.serializeUser(User.serializeUser());  // Add User session storage.
 passport.deserializeUser(User.deserializeUser());  // Remove User sessino storage.
 
 app.use((req, res, next) => {
-    console.log(req.session);
+    console.log(req.body.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
