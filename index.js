b203f2165a9287b2d36fd9152e2bbba9a19f8ae6
Parth <parthwani22@gmail.com>
2024-07-17 14:09:54 +0530

    REmoved query message.

--- index.js
+++ index.js
@@ -128,7 +128,6 @@ passport.serializeUser(User.serializeUser());  // Add User session storage.
 passport.deserializeUser(User.deserializeUser());  // Remove User sessino storage.
 
 app.use((req, res, next) => {
-    console.log(req.body.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
