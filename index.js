50f7d670c0bf67fc4354a630557cd21f44527c2d
Parth <parthwani22@gmail.com>
2024-07-17 10:01:43 +0530

    Removed stacktrace from production env.

--- index.js
+++ index.js
@@ -48,7 +48,9 @@ const sessionConfig = {
     resave: false,
     saveUninitialized: true,
     cookie: {
+        name: 'session',
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
